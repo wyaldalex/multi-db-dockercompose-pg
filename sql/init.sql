a5085ffc31caf0ec4a23de530281fba4be994c75
Alexander Alvarenga <moltron_nano@hotmail.com>
2023-06-04 12:07:11 -0600

    feat: initial setup of multi db docker-compose development example

---
+++ sql/init.sql
@@ -0,0 +1,19 @@
+-- Create tradesdb
+CREATE DATABASE tradesdb;
+
+-- Switch to tradesdb
+\c tradesdb;
+
+-- Create usersdb
+CREATE DATABASE usersdb;
+
+-- Switch to usersdb
+\c usersdb;
+
+
+-- Create reviewsdb
+CREATE DATABASE reviewsdb;
+
+-- Switch to reviewsdb
+\c reviewsdb;
+
